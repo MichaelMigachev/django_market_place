dcca01e4344db79329c4c8717cc9172928b82e2b
MichaelMigachev <migachev21@gmail.com>
2025-03-28 16:58:01 +0300

    * Д/З 24, два контроллера и к ним страницы на добавление продуктов и категорий

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from catalog.models import Product
+from catalog.models import Product, Category
+from django.http import HttpResponse
 
 # def home(request):
 #     return render(request, 'catalog/base.html')
@@ -18,3 +19,43 @@ def product (request, pk):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def add_product(request):
+
+    if request.method == 'POST':
+        name_product = request.POST.get('name_product')
+        category_id = request.POST.get('category')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        image = request.FILES.get('image')
+
+        category = Category.objects.get(id=category_id)
+        product = Product(
+            name_product=name_product,
+            category=category,
+            description=description,
+            price=price,
+            image=image,
+        )
+
+        product.save()
+        response = f"Товар {name_product} успешно добавлен! <a href='/'>Вернуться на главную</a>"
+        return HttpResponse(response)
+    category = Category.objects.all()
+    return render(request, 'catalog/add_product.html', {'categories': category})
+
+
+def add_category(request):
+
+    if request.method == "POST":
+        name_category = request.POST.get('name_category')
+        description = request.POST.get("description")
+
+        category = Category(
+            name_category = name_category,
+            description = description
+        )
+
+        category.save()
+        return HttpResponse(f"Категория {name_category} успешно добавлена! <a href='/'>Вернуться на главную</a>")
+    return render(request, 'catalog/add_category.html')
