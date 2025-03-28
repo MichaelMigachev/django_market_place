4b411ae17aa3c4a5c76f37dcf0a0c7623da441b4
MichaelMigachev <migachev21@gmail.com>
2025-03-28 13:38:52 +0300

    выполнил Д/З 24

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.products_list, name='products_list'),
+    path('product/<int:pk>/', views.product, name='product'),
     path('contacts/', views.contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
+
+# def home(request):
+#     return render(request, 'catalog/base.html')
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/products_list.html', context)
 
 
-def home(request):
-    return render(request, 'catalog/home.html')
+def product (request, pk):
+    product = get_object_or_404(Product, id=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
