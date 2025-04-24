eaf46987da468b7d8f6d4a8e408eb6941c2d3aa6
MichaelMigachev <migachev21@gmail.com>
2025-04-24 16:47:23 +0300

    Выполнил задание 2 и задание 3 из д/з 27

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,10 @@ from catalog.models import Product, Category
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import ProductForm
+
 
 
 class ProductListView(ListView):
@@ -13,24 +15,24 @@ class ProductListView(ListView):
 # app_name/<model_name>_<action>     имя_приложения/<имя_модели>_<действие>
 # catalog/product_list.html
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin,DetailView):
     model = Product
 
 
-class ProductCreateView(CreateView):
-    model = Product
-    form_class = ProductForm
-    template_name = 'catalog/product_form.html'
-    success_url = reverse_lazy('catalog:products_list')
-
-
-class ProductUpdateView(UpdateView):
+class ProductCreateView(LoginRequiredMixin,CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:products_list')
 
-class ProductDeleteView(DeleteView):
+
+class ProductUpdateView(LoginRequiredMixin,UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+class ProductDeleteView(LoginRequiredMixin,DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:products_list')
 
